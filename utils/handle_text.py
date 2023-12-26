5a10ff2a497ecdbc471b0a961aece1cab4c0fa75
Niaev <trutnv@protonmail.com>
2023-12-26 18:04:33 -0300

    New functions file: handle_text.py

---
+++ utils/handle_text.py
@@ -0,0 +1,48 @@
+import re
+
+def transform_periods(t: str):
+    return re.sub(r'[!?]', '.', t)
+
+def transform_numbers(t: str):
+    # Numbers dictionary
+    d = {
+        '0': 'zero ',
+        '1': 'one ',
+        '2': 'two ',
+        '3': 'three ',
+        '4': 'four ',
+        '5': 'five ',
+        '6': 'six ',
+        '7': 'seven ',
+        '8': 'eight ',
+        '9': 'nine '
+    }
+    # List of number in words
+    l_nstr = [d[i] for i in d]
+
+    # Replace number by number in words
+    for i in d:
+        t = t.replace(i, d[i])
+
+    # If there is any number in words that is followed by two spaces 
+    # ('  '), remove one space
+    for n in l_nstr:
+        wrong = n + ' '
+        if wrong in t:
+            t = t.replace(wrong, n)
+
+    return t
+
+def remove_useless_chars(t: str):
+    return re.sub(r'[^a-z. ]', '', t)
+
+def clear_text(t: str):
+    t = t.lower()
+    t = transform_periods(t)
+    t = transform_numbers(t)
+    t = remove_useless_chars(t)
+    return t
+
+if __name__ == '__main__':
+    text = input('Input your text: ')
+    print(clear_text(text))
