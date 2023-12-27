8438e3bd39eb9c97ee7354a9392cea652721f511
Niaev <trutnv@protonmail.com>
2023-12-27 20:16:47 -0300

    New functions file: handle_images.py

---
+++ utils/handle_images.py
@@ -0,0 +1,30 @@
+from PIL import Image
+
+img_folder = 'imgs/'
+abc_img = img_folder + 'alphabet/'
+other_img = img_folder + 'other/'
+
+def get_image(char: str):
+    """Get correspondent image object of the given single char
+
+    Arguments:
+        char {str} -- Single character
+
+    Returns:
+        {PIL.Image.Image} -- Image of the given char
+    """
+
+    ch = char
+    if ch == '.':
+        ch = 'period'
+    elif ch == ' ':
+        ch = 'space'
+
+    img_file = abc_img + ch + '.png'
+    img = Image.open(img_file)
+
+    return img
+
+def border_image():
+    """Get image object of the border"""
+    return Image.open(other_img + 'border.png')
