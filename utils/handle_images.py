5e1ff6bd1772256a31ece9bbf71ca522aca5182f
Niaev <trutnv@protonmail.com>
2023-12-27 22:53:15 -0300

    Added main function 'matrix_to_image'

--- utils/handle_images.py
+++ utils/handle_images.py
@@ -28,3 +28,56 @@ def get_image(char: str) -> 'PIL.Image.Image':
 def border_image() -> 'PIL.Image.Image':
     """Get image object of the border"""
     return Image.open(other_img + 'border.png')
+
+def matrix_to_image(l: list) -> 'PIL.Image.Image':
+    """Get text matrix and transform it to encoded image
+
+    Arguments:
+        l {list} -- Bidimentional array with zeros and single char 
+                    strings
+
+    Returns:
+        PIL.Image.Image: Image that represents the encoded text in
+                         T Code :)
+    """
+
+    h = len(l) * 16
+    w = len(l[0]) * 16
+
+    img = Image.new('RGBA', (w, h), 'WHITE')
+
+    border_img = border_image()
+
+    x_offset = 0
+    y_offset = 0
+    for i in range(len(l)):
+        for j in range(len(l[i])):
+            oc = l[i][j]
+            if oc == 0:
+                x_offset += 16
+                continue
+                
+            c = oc.lower()
+
+            char_img = get_image(c)
+            img.paste(char_img, (x_offset, y_offset))
+
+            if oc == c.upper() and oc != ' ' and oc != '.':
+                img.paste(border_img, (x_offset, y_offset), border_img)
+
+            x_offset += 16
+        
+        x_offset = 0
+        y_offset += 16
+
+    return img
+
+if __name__ == '__main__':
+    from utils.handle_text import clear_text
+    from utils.handle_matrix import get_text_array
+
+    t = input('Text: ')
+    t = clear_text(t)
+    l = get_text_array(t)
+    img = matrix_to_image(l)
+    img.save('Test.png')
